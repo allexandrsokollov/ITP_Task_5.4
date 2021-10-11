f9dcfa89ae9bae24bb2c3ebf83353107002f789e
allexandersokollov <allexandersokollov@gmail.com>
2021-10-11 11:23:32 +0300

    Done

--- src/vsu/csf/sokolov_aa/Main.java
+++ src/vsu/csf/sokolov_aa/Main.java
@@ -1,8 +1,41 @@
 package vsu.csf.sokolov_aa;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+        int h = readIntFromConsole("Enter h ");
+        printFigure(h);
+    }
+
+    /**
+     * h >= 1
+     * h is amount of lines and maximum amount of symbols in particular line
+     */
+    public static void printFigure(int h) {
+        for(int j = 1; j <= h; j++) {
+            for (int i = 1; i <= j; i++) {
+                if (isOdd(i)) {
+                    System.out.print("a");
+                } else {
+                    System.out.print("b");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isOdd(int n) {
+        return n % 2 != 0;
+    }
+
+    public static int readIntFromConsole(String message) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("\n" + message + "   ");
+        return in.nextInt();
     }
 }
